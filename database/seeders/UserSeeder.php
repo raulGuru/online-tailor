5365f38dd187ea080d549e965d844f9dd1e8523c
Abdul Baquee <abdulbaquee@gmail.com>
2022-02-26 11:28:41 +0530

    added user seeder, layout fixed for header, sidebar and footer, added product list page, added product page

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = array(
+            array(
+                'creator' => 1,
+                'first_name' => 'John',
+                'last_name' => "Doe",
+                'email' => 'info@example.com',
+                'password' => Hash::make('admin@123'),
+                'gender' => 'male',
+                'phone' => '1234567890',
+                'pin_code' => '400051',
+                'address' => '2878 Carriage Lane',
+                'status' => 'active',
+                'role' => 'admin'
+            )
+        );
+        User::insert($users);
+    }
+}
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -6,6 +6,110 @@
         <h5 class="card-title mb-0">Empty card</h5>
     </div>
     <div class="card-body">
+        <table class="table table-striped">
+            <thead>
+               <tr>
+                  <th>Product</th>
+                  <th> Product Name </th>
+                  <th> Product Type </th>
+                  <th> Product Price </th>
+                  <th> Product Size </th>
+                  <th>Actions</th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr>
+                  <td>Digital Printed</td>
+                  <td>Slik Fabric </td>
+                  <td>Slik</td>
+                  <td>10,000</td>
+                  <td>2</td>
+                  <td class="table-action">
+                     <a href="#">
+                        <i class="align-middle me-2" data-feather="edit"></i> <span class="align-middle">edit</span>
+                     </a>
+                     <a href="#">
+                        <i class="align-middle me-2" data-feather="trash"></i> <span class="align-middle">trash</span>
+                     </a>
+                  </td>
+               </tr>
+               <tr>
+                  <td>Digital Printed</td>
+                  <td>Slik Fabric </td>
+                  <td>Slik</td>
+                  <td>10,000</td>
+                  <td>2</td>
+                  <td class="table-action">
+                    <a href="#">
+                        <i class="align-middle me-2" data-feather="edit"></i> <span class="align-middle">edit</span>
+                     </a>
+                     <a href="#">
+                        <i class="align-middle me-2" data-feather="trash"></i> <span class="align-middle">trash</span>
+                     </a>
+                  </td>
+               </tr>
+               <tr>
+                  <td>Digital Printed</td>
+                  <td>Slik Fabric </td>
+                  <td>Slik</td>
+                  <td>10,000</td>
+                  <td>2</td>
+                  <td class="table-action">
+                    <a href="#">
+                        <i class="align-middle me-2" data-feather="edit"></i> <span class="align-middle">edit</span>
+                     </a>
+                     <a href="#">
+                        <i class="align-middle me-2" data-feather="trash"></i> <span class="align-middle">trash</span>
+                     </a>
+                  </td>
+               </tr>
+               <tr>
+                  <td>Digital Printed</td>
+                  <td>Slik Fabric </td>
+                  <td>Slik</td>
+                  <td>10,000</td>
+                  <td>2</td>
+                  <td class="table-action">
+                    <a href="#">
+                        <i class="align-middle me-2" data-feather="edit"></i> <span class="align-middle">edit</span>
+                     </a>
+                     <a href="#">
+                        <i class="align-middle me-2" data-feather="trash"></i> <span class="align-middle">trash</span>
+                     </a>
+                  </td>
+               </tr>
+               <tr>
+                  <td>Digital Printed</td>
+                  <td>Slik Fabric </td>
+                  <td>Slik</td>
+                  <td>10,000</td>
+                  <td>2</td>
+                  <td class="table-action">
+                    <a href="#">
+                        <i class="align-middle me-2" data-feather="edit"></i> <span class="align-middle">edit</span>
+                     </a>
+                     <a href="#">
+                        <i class="align-middle me-2" data-feather="trash"></i> <span class="align-middle">trash</span>
+                     </a>
+                  </td>
+               </tr>
+               <tr>
+                  <td>Digital Printed</td>
+                  <td>Slik Fabric </td>
+                  <td>Slik</td>
+                  <td>10,000</td>
+                  <td>2</td>
+                  <td class="table-action">
+                    <a href="#">
+                        <i class="align-middle me-2" data-feather="edit"></i> <span class="align-middle">edit</span>
+                     </a>
+                     <a href="#">
+                        <i class="align-middle me-2" data-feather="trash"></i> <span class="align-middle">trash</span>
+                     </a>
+                  </td>
+               </tr>
+            </tbody>
+         </table>
     </div>
 </div>
 @endsection
