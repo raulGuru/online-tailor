95b30052f4661edc64ab2e76b74c5dbe8f6a64cc
Abdul Baquee <abdulbaquee@gmail.com>
2022-02-26 15:49:59 +0530

    created seeders for category, color, types, sizes, sleeve, product & created models for the same

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $products = array(
+            array(
+                'creator' => 1,
+                'cat_id' => 1,
+                'color_id' => 1,
+                'size_id' => 1,
+                'type_id' => 1,
+                'sleeve_id' => 1,
+                'title' => 'On Cloud Nine Pillow',
+                'slug' => 'On-cloud-nine-pillow',
+                'description' => "Sociosqu facilisis duis ...",
+                'additional_details' => 'This is an example to demonstrate how product variations can be used to allow customers to select options e.g. size/colour when buying products.',
+                'sku' => "XYZ-PQR-ABC",
+                'price' => 100,
+                'images' => json_encode([1, 2, 3, 4], true),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            )
+        );
+        Product::truncate();
+        Product::insert($products);
+    }
+}
