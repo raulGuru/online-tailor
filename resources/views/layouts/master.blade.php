89306669be6e2f8ed0dd2a100371ae9741cb964a
Abdul Baquee <abdulbaquee@gmail.com>
2022-04-11 21:53:53 +0530

    added login button into header file

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,6 +14,7 @@
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
       <link href="{{ asset('assets/css/light.css') }}" rel="stylesheet">
       <link href="{{ asset('assets/css/admin.css') }}" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
    </head>
    <body data-theme="default" data-layout="fluid" data-sidebar-position="left" data-sidebar-layout="default">
       <div class="wrapper">
