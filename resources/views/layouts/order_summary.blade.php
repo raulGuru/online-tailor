16a86965a16376deaa1b8892bc9570fd1cdf2ce9
maheshk_tech <maheshkahar75@gmail.com>
2022-12-03 08:33:31 +0530

    Summary accordion html updated and removed extraa script

--- resources/views/layouts/order_summary.blade.php
+++ resources/views/layouts/order_summary.blade.php
@@ -20,44 +20,45 @@
 
     <div class="row">
         <div class="col-md-8">
-            <div id="accordion">
-                <div class="card-acc">
-                    <div class="card-header d-flex justify-content-between" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true">
-                        <span class="title font-weight-500">Product Details</span>
-                        <span class="accicon"><i class="fa fa-angle-down rotate-icon"></i></span>
-                    </div>
-                    <div id="collapseOne" class="collapse show" data-parent="#accordion">
-                        <div class="card-body product-summary-box">
+            <div class="accordion" id="accordionPanelsStayOpenExample">
+                <div class="accordion-item card-acc">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                        <button class="accordion-button title font-weight-500" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                            Product Details
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                        <div class="accordion-body">
                             <ul>
                                 @foreach ($products as $product)
-                                    <li class="mb-2 pb-2 bd-bottom d-flex justify-content-between">
-                                        <div class="d-flex">
-                                            <div class="Summary-img-box me-2">
-                                                <img src="{{ asset('public/products/'.$product->images) }}" alt="">
-                                            </div>
-                                            <div>
-                                                <h3>{{ucwords($product->title)}}</h3>
-                                                <p class="mb-1">Delivery by {{$deliver_by['format']}}</p>
-                                                <h6 class="text-brown f-20 font-weight-500 m-0"> ₹{{$product->price}}</h6>
-                                            </div>
+                                <li class="mb-2 pb-2 bd-bottom d-flex justify-content-between">
+                                    <div class="d-flex">
+                                        <div class="Summary-img-box me-2">
+                                            <img src="{{ asset('public/products/'.$product->images) }}" alt="">
                                         </div>
-                                        <a href="">
-                                            <img src="{{ asset('public/assets/img/cancel.svg') }}" alt="">
-                                        </a>
-                                    </li>
+                                        <div>
+                                            <h3>{{ucwords($product->title)}}</h3>
+                                            <p class="mb-1">Delivery by {{$deliver_by['format']}}</p>
+                                            <h6 class="text-brown f-20 font-weight-500 m-0"> ₹{{$product->price}}</h6>
+                                        </div>
+                                    </div>
+                                    <a href="">
+                                        <img src="{{ asset('public/assets/img/cancel.svg') }}" alt="">
+                                    </a>
+                                </li>
                                 @endforeach
                             </ul>
                         </div>
                     </div>
                 </div>
-
-                <div class="card-acc">
-                    <div class="card-header d-flex justify-content-between" data-toggle="collapse" href="#collapseTwo">
-                        <span class="title font-weight-500">Tailor Details</span>
-                        <span class="accicon"><i class="fa fa-angle-down rotate-icon"></i></span>
-                    </div>
-                    <div id="collapseTwo" class="collapse" data-parent="#accordion">
-                        <div class="card-body">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                        <button class="accordion-button collapsed title font-weight-500" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                            Tailor Details
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                        <div class="accordion-body">
                             <div class="tailor-details-box d-flex">
                                 <div class="tailor-photo">
                                     <img src="{{ asset('public/tailors/'.$tailor->photos) }}" alt="">
@@ -73,10 +74,8 @@
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
-
         <div class="col-md-4">
             <div class="card-acc mb-3">
                 <div class="card-header">
@@ -103,14 +102,13 @@
                     </table>
                 </div>
             </div>
-
             <div class="card-acc mb-3">
                 <div class="card-header">
                     <span class="title font-weight-500">Card Details</span>
                 </div>
                 <div class="box-body p-2">
                     <img class="mt-3 mb-3" src="{{ asset('public/assets/img/multiple_cards-icon.svg')}}" alt="">
-                   
+
                     <form>
                         <div class="row mb-3">
                             <div class="col-md-12">
