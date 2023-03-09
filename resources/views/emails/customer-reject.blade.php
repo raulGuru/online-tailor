c26d4016d497d936566799dbb07ceeb9a438da86
rahulg <basicallyrahulg@gmail.com>
2023-03-09 21:10:24 +0530

    comment line

--- resources/views/emails/customer-reject.blade.php
+++ resources/views/emails/customer-reject.blade.php
@@ -7,7 +7,7 @@
     <h1>Hello {{ $data['body']['customer_name'] }},</h1>
     <p style="margin-top: 15px;">We are processing your request and you will receive an appointment details soon</p>
     <p style="margin-top: 15px;">visit : <a href="https://bookmytailor.in">bookmytailor.in</a></p>
-    <p style="margin-top: 15px;">If you do not wish to receive these emails kindly click unsubscribe link.</p>
+    <!-- <p style="margin-top: 15px;">If you do not wish to receive these emails kindly click unsubscribe link.</p> -->
     <p style="margin-top: 40px;">Thanks & Regards!</p>
 </body>
 </html>
